03f838683dbed62989a65f92bd413bdf2bb6a6cf
raza jafri <razajafri@Mac.attlocal.net>
2025-12-26 19:42:31 -0600

    asf

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,9 +1,11 @@
 import place from './place'
 import neighborhood from './neighborhood'
 import guide from './guide'
+import post from './post'
 
 export const schemaTypes = [
   place,
   neighborhood,
   guide,
+  post,
 ]
---
+++ schemaTypes/post.ts
@@ -0,0 +1,40 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 200,
+      },
+      validation: Rule => Rule.required(),
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'text',
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published At',
+      type: 'datetime',
+    }),
+  ],
+  preview: {
+    select: {
+      title: 'title',
+    },
+  },
+})
