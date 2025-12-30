b11056243e854c9bf09aa1e0a6945903d7108b52
raza jafri <razajafri@Mac.attlocal.net>
2025-12-30 13:27:58 -0600

    assss

--- interesting-austin-blog/app/(blog)/guides/page.tsx
+++ interesting-austin-blog/app/(blog)/guides/page.tsx
@@ -35,20 +35,22 @@ async function AllGuides() {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
-      {guides.map((guide) => (
-        <GuideCard
-          key={guide._id}
-          _id={guide._id}
-          title={guide.title}
-          slug={guide.slug}
-          content={guide.content}
-          guideType={guide.guideType}
-          img={guide.img}
-          updatedAt={guide.updatedAt}
-          places={guide.places}
-          neighborhoods={guide.neighborhoods}
-        />
-      ))}
+      {guides
+        .filter((guide) => guide.title && guide.slug)
+        .map((guide) => (
+          <GuideCard
+            key={guide._id}
+            _id={guide._id}
+            title={guide.title!}
+            slug={guide.slug!}
+            content={guide.content}
+            guideType={guide.guideType}
+            img={guide.img}
+            updatedAt={guide.updatedAt}
+            places={guide.places}
+            neighborhoods={guide.neighborhoods}
+          />
+        ))}
     </div>
   );
 }
